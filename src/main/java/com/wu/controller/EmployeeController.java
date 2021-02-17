d5716d44393f2229bff45f777a89165f19930008
2nc <wnchit@163.com>
2021-02-17 18:49:54 -0500

    without optimize

--- src/main/java/com/wu/controller/EmployeeController.java
+++ src/main/java/com/wu/controller/EmployeeController.java
@@ -1,13 +1,24 @@
 package com.wu.controller;
 
+import com.wu.dao.EmployeeDao;
+import com.wu.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
 
 @Controller
 public class EmployeeController {
 
-    @RequestMapping("tables.html")
-    public String list(){
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @RequestMapping("employ")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("allEmployees", employees);
         return "tables";
     }
 }
