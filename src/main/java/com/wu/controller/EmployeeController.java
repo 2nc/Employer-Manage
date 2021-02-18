3d42cc4d876018453bed3149503d081bcf131def
2nc <wnchit@163.com>
2021-02-18 16:23:09 -0500

    finish add employee

--- src/main/java/com/wu/controller/EmployeeController.java
+++ src/main/java/com/wu/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package com.wu.controller;
 
+import com.wu.dao.DepartmentDao;
 import com.wu.dao.EmployeeDao;
+import com.wu.pojo.Department;
 import com.wu.pojo.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collection;
@@ -14,11 +18,28 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
-    @RequestMapping("employ")
+
+    @RequestMapping("/employ")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("allEmployees", employees);
         return "tables";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("departments",departments);
+        return "addEmployee";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        System.out.println("save=>" + employee);
+        employeeDao.addEmployee(employee);
+        return "redirect:/employ";
     }
 }
